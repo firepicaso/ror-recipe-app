18748bd80dae6c6595e9ca731d61c13654720a6b
Mustakim <m.m.kiron@gmail.com>
2023-11-23 13:46:24 +0600

    Add User, Recipe and RecipeFood controllers and views

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  resources :recipes, only: [:index, :new, :create, :show, :update, :destroy] do
+    resources :recipe_foods
+  end
 end
