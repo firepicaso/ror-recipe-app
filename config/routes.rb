5b6f5a2a513358449c262b7ebc897b3d85b7c132
unknown <albertkantwi@gmail.com>
2023-11-23 13:00:33 +0000

    Add cotrollers#inventories_controller.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
-Rails.application.routes.draw do
+Rails.application.routes.draw
+  root to: "foods#index"
+  resources :foods, only: [:index, :new, :create, :show, :edit, :update, :destroy
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -7,4 +9,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  resources :recipes, only: [:index, :new, :create, :show, :update, :destroy] do
+    resources :recipe_foods
 end
