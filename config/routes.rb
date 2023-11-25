849eb1989bc243fd7fac22915ec3ddcf41279baa
unknown <albertkantwi@gmail.com>
2023-11-25 15:05:56 +0000

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,34 +1,35 @@
 Rails.application.routes.draw do
   devise_for :users
-  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get '/inventories', to: 'inventories#index'
-  get '/inventories/new', to: 'inventories#new'
-  post '/inventories', to: 'inventories#create'
-  get '/inventories/:id', to: 'inventories#show'
-  delete '/inventories/:id', to: 'inventories#destroy'
-  get '/inventories/:id/inventory_foods/new', to: 'inventory_foods#new'
-  post '/inventories/:id/inventory_foods', to: 'inventory_foods#create'
-  delete '/inventory_foods/:id', to: 'inventory_foods#destroy'
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  # get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  root "foods#index"
 
-  resources :users
-
-  resources :recipes, only: [:index, :new, :create, :show, :update, :destroy] do
-    resources :recipe_foods, only: [:new, :create, :edit, :update, :destroy]
+  devise_scope :user do
+    authenticated :user do
+      root :to => "foods#index", as: :authenticated_root
+      get '/users/sign_out' => 'devise/sessions#destroy'
+    end
+    unauthenticated :user do
+      root :to => "devise/sessions#new", as: :unauthenticated_root
+    end
   end
-  
-  resources :public_recipes, only: [:index]
-  
-  resources :inventories, only: [:index, :show, :new, :create, :destroy] do
+
+  resources :users, only: [:index]
+  resources :foods, except: [:update]
+  resources :public_recipes,except: [:update]
+  resources :shopping_lists, except: [:update]
+
+  resources :recipes do
+    resources :recipe_foods, only: [:create, :destroy]
+  end
+
+  resources :inventories, except: [:update] do
     resources :inventory_foods, only: [:new, :create, :destroy]
-  end
-  
-  resources :foods, only: [:index, :new, :create, :edit, :destroy]
+  end 
 
-  resources :shopping_lists, only: [:index, :show, :new, :create, :edit, :update, :destroy]
-
-  resources :general_shopping_list, only: [:index]
+  get 'shopping_lists/index'
 end
