1c70624cf7aea5d4076a0e1a5cd574c0ef416b97
Mustakim <m.m.kiron@gmail.com>
2023-11-26 02:35:58 +0600

    Add integration tests

--- spec/integration/food_index_spec.rb
+++ spec/integration/food_index_spec.rb
@@ -9,19 +9,38 @@ RSpec.describe 'Food index page', type: :feature do
     fill_in 'Password', with: '123456'
     click_button 'Log in'
 
+    # Create some sample foods for the user
+    @food1 = @user1.foods.create(food: 'Apple', measurement_unit: 'kg', price: 15)
+    @food2 = @user1.foods.create(food: 'Banana', measurement_unit: 'each', price: 10)
+
     visit '/foods'
   end
 
-  it 'should have the following content' do
-    expect(page).to have_content('Hi Albert Antwi')
+  it 'should have the greetings' do
     expect(page).to have_content('Food List')
     expect(page).to have_link('Foods')
     expect(page).to have_link('Recipes')
-    expect(page).to have_content('Please add foods using the Add Food button')
+    expect(page).to have_content('Add Food')
   end
 
-  it 'should have the following content' do
+  it 'should have the Add food link' do
     click_link 'Add Food'
     expect(current_path).to eq(new_food_path)
+  end
+
+  it 'should display existing foods' do
+    expect(page).to have_content('Apple')
+    expect(page).to have_content('Banana')
+  end
+
+  it 'should display delete link for user\'s foods' do
+    expect(page).to have_content('Delete', count: 2) # Assuming two foods are created in the before block
+  end
+
+  it 'should delete a food', js: true do
+    accept_confirm do
+      first(:link, 'Delete').click
+    end
+    expect(page).not_to have_content('Apple')
   end
 end
