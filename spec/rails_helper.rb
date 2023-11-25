2fc55033b38c60667b92da126bae686d63fe06f0
Mustakim <m.m.kiron@gmail.com>
2023-11-26 00:27:33 +0600

    Install database cleaner

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -35,6 +35,17 @@ RSpec.configure do |config|
     Rails.root.join('spec/fixtures')
   ]
 
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
+
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
