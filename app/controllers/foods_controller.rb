cc18232f0b364c305e2de66192c1a7fd13d37e73
mumbereh <siirasonh@gmail.com>
2023-11-28 02:38:03 +0300

    Add User controller

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,12 +1,12 @@
-# app/controllers/foods_controller.rb
 class FoodsController < ApplicationController
-  before_action :set_food, only: [:show, :edit, :update, :destroy]
+  load_and_authorize_resource
 
   def index
     @foods = Food.all
   end
 
   def show
+    @food = Food.find(params[:id])
   end
 
   def new
@@ -14,38 +14,30 @@ class FoodsController < ApplicationController
   end
 
   def create
-    @food = Food.new(food_params)
+    @food = current_user.foods.new(food_params)
 
-    if @food.save
-      redirect_to @food, notice: 'Food was successfully created.'
-    else
-      render :new
-    end
-  end
-
-  def edit
-  end
-
-  def update
-    if @food.update(food_params)
-      redirect_to @food, notice: 'Food was successfully updated.'
-    else
-      render :edit
+    respond_to do |format|
+      if @food.save
+        format.html { redirect_to foods_path, notice: 'Food was created successfully' }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
     end
   end
 
   def destroy
-    @food.destroy
-    redirect_to foods_url, notice: 'Food was successfully destroyed.'
+    @food = Food.find(params[:id])
+    authorize! :destroy, @food
+    @food.destroy!
+
+    respond_to do |format|
+      format.html { redirect_to foods_url, notice: 'Food was successfully deleted.' }
+    end
   end
 
   private
 
-  def set_food
-    @food = Food.find(params[:id])
-  end
-
   def food_params
-    params.require(:food).permit(:name, :measurement_unit, :price)
+    params.require(:food).permit(:food, :measurement_unit, :price)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: "foods#index"
   
-  resources :foods, only: [:index, :new, :create, :show, :edit, :update, :destroy] do
+  resources :foods, only: [:index,  :create, :show, :edit, :update, :destroy] do
     # additional routes or constraints can go here if needed
   end
 end
