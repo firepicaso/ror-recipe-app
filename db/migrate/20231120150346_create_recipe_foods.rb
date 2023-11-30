184cf33c52d36d97ca14ca921a64d438af107d9e
Mustakim <m.m.kiron@gmail.com>
2023-11-30 13:36:55 +0600

    Update migration files

--- db/migrate/20231120150346_create_recipe_foods.rb
+++ db/migrate/20231120150346_create_recipe_foods.rb
@@ -2,8 +2,8 @@ class CreateRecipeFoods < ActiveRecord::Migration[7.1]
   def change
     create_table :recipe_foods do |t|
       t.integer :quantity
-      add_reference :recipe_foods, :recipe, null: false, foreign_key: true
-      add_reference :recipe_foods, :food, null: false, foreign_key: true
+      add_reference :recipe, null: false, foreign_key: true
+      add_reference :food, null: false, foreign_key: true
 
       t.timestamps
     end
