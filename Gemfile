dbfd399c9992eef099e2d564349ba302ebdb8232
Mustakim <m.m.kiron@gmail.com>
2023-12-01 17:47:51 +0600

    Install rspec and capybara

--- Gemfile
+++ Gemfile
@@ -47,6 +47,8 @@ gem 'bootsnap', require: false
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem 'debug', platforms: %i[mri windows]
+  gem 'rspec-rails'
+  gem 'capybara'
 end
 
 group :development do
@@ -58,6 +60,11 @@ group :development do
 
   # Speed up commands on slow machines / big apps [https://github.com/rails/spring]
   # gem "spring"
+end
+
+group :test do  
+  gem 'database_cleaner'
+  gem 'selenium-webdriver'
 end
 
 gem 'rubocop', '>= 1.0', '< 2.0'
---
+++ spec/support/database_cleaner.rb
@@ -0,0 +1,26 @@
+# spec/support/database_cleaner.rb
+RSpec.configure do |config|
+    config.before(:suite) do
+      DatabaseCleaner.clean_with(:truncation)
+    end
+  
+    config.before(:each) do
+      DatabaseCleaner.strategy = :transaction
+    end
+  
+    config.before(:each, type: :feature) do
+      # :rack_test driver's Rack app under test shares database connection
+      # with the specs, so we have to use a different strategy for
+      # cleaning.
+      DatabaseCleaner.strategy = :truncation
+    end
+  
+    config.before(:each) do
+      DatabaseCleaner.start
+    end
+  
+    config.after(:each) do
+      DatabaseCleaner.clean
+    end
+  end
+  
