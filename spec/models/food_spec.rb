f48f59f38f75c45a0b09f1a6aa37f3c14e4dcb5a
Mustakim <m.m.kiron@gmail.com>
2023-12-01 18:28:37 +0600

    Update food unit tests

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -14,17 +14,22 @@ RSpec.describe Food, type: :model do
   end
 
   it 'is not valid without a name' do
-    food = Food.new(measurement_unit: 'kg', price: 5)
+    food = Food.new(measurement_unit: 'kg', price: 5, quantity: 1)
     expect(food).not_to be_valid
   end
 
   it 'is not valid without a measurement unit' do
-    food = Food.new(name: 'apple', price: 5)
+    food = Food.new(name: 'apple', price: 5, quantity: 1)
     expect(food).not_to be_valid
   end
 
   it 'is not valid without a price' do
-    food = Food.new(name: 'apple', measurement_unit: 'kg')
+    food = Food.new(name: 'apple', measurement_unit: 'kg', quantity: 1)
+    expect(food).not_to be_valid
+  end
+
+  it 'is not valid without a quantity' do
+    food = Food.new(name: 'apple', measurement_unit: 'kg', price: 5)
     expect(food).not_to be_valid
   end
 end
